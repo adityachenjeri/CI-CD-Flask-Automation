af03a5444f23c56232cd0135b6788eca458b2482
Aditya sai chenjeri <adityasai0330@gmail.com>
2025-02-24 01:42:19 +0530

    CI/CD

--- test_app.py
+++ test_app.py
@@ -1,6 +1,13 @@
 import pytest
 from app import app
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use the port provided by Vercel
+    app.run(host="0.0.0.0", port=port)
+
+
 @pytest.fixture
 def client():
     """Create a test client for the Flask app"""
